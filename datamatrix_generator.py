c2100f9f6ad9883352fe335e62a2d31396c2dc6c
CheesePancake <92983457+CheesePancake@users.noreply.github.com>
2023-07-31 21:33:49 +0800

    Update datamatrix_generator.py

--- datamatrix_generator.py
+++ datamatrix_generator.py
@@ -1,6 +1,6 @@
 from pylibdmtx.pylibdmtx import encode, decode
 from PIL import Image
 encoded = encode('hello world'.encode('utf8'))
-img = Image.frombytes('RGB', (100, 100), encoded.pixels)
+img = Image.frombytes('RGB', (encoded.height, encoded.width), encoded.pixels)
 img.save('dmtx.png')
 print(decode(Image.open('dmtx.png')))
