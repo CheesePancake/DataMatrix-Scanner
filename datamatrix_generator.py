7ff9fadaca0ce9d5f7178d4a4a805c63cd6a6059
CheesePancake <92983457+CheesePancake@users.noreply.github.com>
2023-07-31 13:59:22 +0800

    Fix wrong datatype

--- datamatrix_generator.py
+++ datamatrix_generator.py
@@ -1,6 +1,6 @@
 from pylibdmtx.pylibdmtx import encode, decode
 from PIL import Image
 encoded = encode('hello world'.encode('utf8'))
-img = Image.frombytes('RGB', (100, 100), 16)
+img = Image.frombytes('RGB', (100, 100), encoded.pixels)
 img.save('dmtx.png')
 print(decode(Image.open('dmtx.png')))
